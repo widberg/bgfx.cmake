35626ebded7d7f08c68ac2edc8328b2189140b17
Andy Duplain <trojanfoe@gmail.com>
2019-06-03 08:40:28 +0100

    Update to latest bgdx and bx.
    Update submodules.
    Drop ib-compress and forsyth-too 3rdparty libraries.
    Add meshoptimizer to geometryc.

--- cmake/tools/geometryc.cmake
+++ cmake/tools/geometryc.cmake
@@ -10,13 +10,12 @@
 
 include( CMakeParseArguments )
 
-include( cmake/3rdparty/forsyth-too.cmake )
-include( cmake/3rdparty/ib-compress.cmake )
+include( cmake/3rdparty/meshoptimizer.cmake )
 
 add_executable( geometryc ${BGFX_DIR}/tools/geometryc/geometryc.cpp )
 target_compile_definitions( geometryc PRIVATE "-D_CRT_SECURE_NO_WARNINGS" )
 set_target_properties( geometryc PROPERTIES FOLDER "bgfx/tools" )
-target_link_libraries( geometryc bx bgfx-bounds bgfx-vertexdecl forsyth-too ib-compress )
+target_link_libraries( geometryc bx bgfx-bounds bgfx-vertexdecl meshoptimizer )
 if( BGFX_CUSTOM_TARGETS )
 	add_dependencies( tools geometryc )
 endif()
