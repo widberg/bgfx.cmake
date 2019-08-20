cd9ee4a5d36e2c618193f68a690aea50fdcf14e9
Aleksandr Knorre <a_knorre@wargaming.net>
2019-08-20 13:28:36 +0300

    bgfx Kinda works

--- cmake/tools/shaderc.cmake
+++ cmake/tools/shaderc.cmake
@@ -20,6 +20,7 @@ add_executable( shaderc ${BGFX_DIR}/tools/shaderc/shaderc.cpp ${BGFX_DIR}/tools/
 target_compile_definitions( shaderc PRIVATE "-D_CRT_SECURE_NO_WARNINGS" )
 set_target_properties( shaderc PROPERTIES FOLDER "bgfx/tools" )
 target_link_libraries( shaderc bx bimg bgfx-vertexdecl bgfx-shader-spirv fcpp glsl-optimizer glslang spirv-cross spirv-tools )
+
 if( BGFX_CUSTOM_TARGETS )
 	add_dependencies( tools shaderc )
 endif()
