6e7dd494d4c1e0c79b7571abc9d71acb948a64ea
Pablo Escobar <mail@rvrs.in>
2020-04-02 16:21:13 +0200

    Update bgfx, bimg, bx

--- cmake/shared.cmake
+++ cmake/shared.cmake
@@ -8,11 +8,11 @@
 # You should have received a copy of the CC0 Public Domain Dedication along with
 # this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-add_library( bgfx-vertexdecl INTERFACE )
-configure_file( ${CMAKE_CURRENT_SOURCE_DIR}/generated/vertexdecl.cpp.in
-                ${CMAKE_CURRENT_BINARY_DIR}/generated/vertexdecl.cpp )
-target_sources( bgfx-vertexdecl INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/generated/vertexdecl.cpp )
-target_include_directories( bgfx-vertexdecl INTERFACE ${BGFX_DIR}/include )
+add_library( bgfx-vertexlayout INTERFACE )
+configure_file( ${CMAKE_CURRENT_SOURCE_DIR}/generated/vertexlayout.cpp.in
+                ${CMAKE_CURRENT_BINARY_DIR}/generated/vertexlayout.cpp )
+target_sources( bgfx-vertexlayout INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/generated/vertexlayout.cpp )
+target_include_directories( bgfx-vertexlayout INTERFACE ${BGFX_DIR}/include )
 
 add_library( bgfx-shader-spirv INTERFACE )
 configure_file( ${CMAKE_CURRENT_SOURCE_DIR}/generated/shader_spirv.cpp.in
@@ -31,5 +31,5 @@ target_include_directories( bgfx-bounds INTERFACE ${BGFX_DIR}/examples/common )
 if( APPLE AND NOT IOS)
 	find_library( COCOA_LIBRARY Cocoa )
 	mark_as_advanced( COCOA_LIBRARY )
-	target_link_libraries( bgfx-vertexdecl INTERFACE ${COCOA_LIBRARY} )
+	target_link_libraries( bgfx-vertexlayout INTERFACE ${COCOA_LIBRARY} )
 endif()
